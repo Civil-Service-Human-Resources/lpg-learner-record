a4fbf2501be259e2b500e07ecf689281e51e7128
Max Stewart <max@cautionyourblast.com>
2018-03-20 10:19:22 +0000

    Adding ModuleRecord, oops :).

---
+++ src/main/java/uk/gov/cslearning/record/domain/ModuleRecord.java
@@ -0,0 +1,83 @@
+package uk.gov.cslearning.record.domain;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
+import java.time.LocalDateTime;
+
+public class ModuleRecord {
+
+    private String moduleId;
+
+    private String eventId;
+
+    private State state;
+
+    private String preference;
+
+    private Result result;
+
+    private String score;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime completionDate;
+
+    public ModuleRecord() {
+    }
+
+    public String getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(String moduleId) {
+        this.moduleId = moduleId;
+    }
+
+    public String getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(String eventId) {
+        this.eventId = eventId;
+    }
+
+    public State getState() {
+        return state;
+    }
+
+    public void setState(State state) {
+        this.state = state;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    public void setResult(Result result) {
+        this.result = result;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public LocalDateTime getCompletionDate() {
+        return completionDate;
+    }
+
+    public void setCompletionDate(LocalDateTime completionDate) {
+        this.completionDate = completionDate;
+    }
+
+    public String getPreference() {
+        return preference;
+    }
+
+    public void setPreference(String preference) {
+        this.preference = preference;
+    }
+}
