202b3721ba28d5802b559379d172f1f22de20774
matthewmu <matthewmu@kainos.com>
2018-05-21 13:44:38 +0100

    adding more learning job stuff

--- src/main/java/uk/gov/cslearning/record/service/RegistryService.java
+++ src/main/java/uk/gov/cslearning/record/service/RegistryService.java
@@ -3,12 +3,25 @@ package uk.gov.cslearning.record.service;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.gov.cslearning.record.service.identity.IdentityService;
 
+@Service
 public class RegistryService {
     private static final Logger LOGGER = LoggerFactory.getLogger(ActivityRecordService.class);
 
+    @Autowired
+    private IdentityService identityService;
 
     @Autowired
     public RegistryService() {
+    }
+
+    public void getProfileById(String id){
+        Object[] response = identityService.httpGet("http://localhost:9002/civilServants/" + id);
+        for (Object o: response){
+            System.out.println(o);
+            System.out.println("");
+        }
     }
 }
