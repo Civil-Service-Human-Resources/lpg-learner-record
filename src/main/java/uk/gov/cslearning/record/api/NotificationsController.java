73fcb2e259f9da8f46e5ba24b2e5803adc4877dd
matthewmu <matthewmu@kainos.com>
2018-06-07 15:04:00 +0100

    fix/exposing-notifications

--- src/main/java/uk/gov/cslearning/record/api/NotificationsController.java
+++ src/main/java/uk/gov/cslearning/record/api/NotificationsController.java
@@ -2,6 +2,7 @@ package uk.gov.cslearning.record.api;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,19 +10,23 @@ import org.springframework.web.bind.annotation.RestController;
 import uk.gov.cslearning.record.service.scheduler.LearningJob;
 import uk.gov.service.notify.NotificationClientException;
 
-import static org.springframework.http.HttpStatus.OK;
-
 @RestController
 @RequestMapping("/notifications")
 @Profile("test")
-public class TestNotificationsController {
+public class NotificationsController {
 
+    /*
+     * Matt - This class will only be available if the 'test' profile is added to the application config/Terraform.
+     * By hitting this URL, we will manually kick of the notifications job which is currently on the scheduler.
+     * Currently just used for development/testing purposes,
+     * but we may choose to extend this class out with more functionality at a later date.
+     * */
     @Autowired
     private LearningJob learningJob;
 
     @GetMapping
     public ResponseEntity runNotifications() throws NotificationClientException {
         learningJob.sendNotificationForIncompleteCourses();
-        return new ResponseEntity<>(OK);
+        return new ResponseEntity<>("Notifications job manually executed.", HttpStatus.OK);
     }
 }
