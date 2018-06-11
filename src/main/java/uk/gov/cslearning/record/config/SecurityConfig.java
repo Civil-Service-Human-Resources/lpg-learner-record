a58fd82263c782fa4e90486ae8a50c152a0ae8cb
Max Stewart <max@cautionyourblast.com>
2018-06-11 15:24:41 +0100

    Update security config for /notifications calls.

--- src/main/java/uk/gov/cslearning/record/config/SecurityConfig.java
+++ src/main/java/uk/gov/cslearning/record/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/notifications");
+        web.ignoring().antMatchers("/notifications/**");
     }
 
     @Bean
