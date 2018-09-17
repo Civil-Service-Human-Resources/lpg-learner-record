6090653faf8c9d63aace675731cc63fb90d7fc02
matthewmu <matthewmu@kainos.com>
2018-09-17 14:53:10 +0100

    LPFG-768: changing learning catalogue calls to use oauth

--- src/main/java/uk/gov/cslearning/record/service/catalogue/LearningCatalogueService.java
+++ src/main/java/uk/gov/cslearning/record/service/catalogue/LearningCatalogueService.java
@@ -1,11 +1,13 @@
 package uk.gov.cslearning.record.service.catalogue;
 
-import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,25 +24,26 @@ public class LearningCatalogueService {
 
     private String requiredLearningUrlFormat;
 
-    private String credentials;
+    private HttpHeaders headers;
 
     public LearningCatalogueService(RestTemplate restTemplate,
-                                    @Value("${catalogue.username}") String username,
-                                    @Value("${catalogue.password}") String password,
                                     @Value("${catalogue.courseUrlFormat}") String courseUrlFormat,
                                     @Value("${catalogue.requiredLearningUrlFormat}") String requiredLearningUrlFormat) {
+
         this.restTemplate = restTemplate;
         this.requiredLearningUrlFormat = requiredLearningUrlFormat;
-        this.credentials = Base64.encodeBase64String((username + ":" + password).getBytes());
         this.courseUrlFormat = courseUrlFormat;
     }
 
     public List<Course> getRequiredCoursesByDepartmentCode(String departmentId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Basic " + credentials);
+        headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + details.getTokenValue());
 
         HttpEntity<String> request = new HttpEntity<>(headers);
+
         ResponseEntity<Results> responseEntity = restTemplate.exchange(
                 String.format(requiredLearningUrlFormat, departmentId),
                 HttpMethod.GET, request, Results.class);
@@ -54,9 +57,11 @@ public class LearningCatalogueService {
     }
 
     public Course getCourse(String courseId) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add("Authorization", "Basic " + credentials);
+        headers = new HttpHeaders();
+        headers.add(HttpHeaders.AUTHORIZATION, "Bearer " + details.getTokenValue());
 
         HttpEntity<String> request = new HttpEntity<>(headers);
 
