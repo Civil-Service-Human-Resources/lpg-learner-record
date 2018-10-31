3cfcbe3cac75b3a8855049f226b8038accd28bea
Peter Taylor <petertaylor@Malekith.local>
2018-10-31 11:25:17 +0000

    LPFG-940: db migration, domain and repository classes

---
+++ src/main/java/uk/gov/cslearning/record/domain/Event.java
@@ -0,0 +1,49 @@
+package uk.gov.cslearning.record.domain;
+
+import javax.persistence.*;
+
+import static com.google.gson.internal.$Gson$Preconditions.checkArgument;
+
+@Entity
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String path;
+
+    public Event(){
+
+    }
+
+    public Event(Long id){
+        checkArgument(id >= 0);
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public String toString() {
+        return "Event{" +
+                "id=" + id +
+                ", path='" + path + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/uk/gov/cslearning/record/repository/EventRepository.java
@@ -0,0 +1,10 @@
+package uk.gov.cslearning.record.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import uk.gov.cslearning.record.domain.Event;
+
+@Repository
+public interface EventRepository extends CrudRepository<Event, Long> {
+
+}
