a8bb9492febc48c396d3cf7c08f8375a30efe84e
Rob Leney <roble@kainos.com>
2018-11-05 17:10:08 +0000

    LPFG-944: Added cascade types on Booking relationships.

--- src/main/java/uk/gov/cslearning/record/domain/Booking.java
+++ src/main/java/uk/gov/cslearning/record/domain/Booking.java
@@ -17,11 +17,11 @@ public class Booking {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinColumn(name="learnerId")
     private Learner learner;
 
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH})
     @JoinColumn(name="eventId")
     private Event event;
 
