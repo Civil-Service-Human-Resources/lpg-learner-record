39f02807c72373fb11522aea74932d728b61e3b4
Rob Leney <roble@kainos.com>
2018-11-07 16:16:00 +0000

    LPFG-924: Fixed syntax errors and added unique keys to event and booking tables;

--- src/main/resources/db/migration/mysql/V1.1.5__add-event-learner-booking-tables.sql
+++ src/main/resources/db/migration/mysql/V1.1.5__add-event-learner-booking-tables.sql
@@ -2,7 +2,8 @@ CREATE TABLE `event` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `event_uid` varchar(60) NOT NULL,
   `path` varchar(255) NOT NULL,
-  PRIMARY KEY(`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_key_catalogue_id` (`catalogue_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `learner` (
@@ -16,9 +17,12 @@ CREATE TABLE `booking` (
   `learner_id` int(10) unsigned NOT NULL,
   `event_id` int(10) unsigned NOT NULL,
   `booking_time` datetime NOT NULL,
-  `status` enum('REQUESTED', 'CONFIRMED'),
-  `payment_details` varchar(255),
-  CONSTRAINT `FK_booking_learnerId_learner_id` FOREIGN KEY(`learner_id`) REFERENCES `learner`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `FK_booking_eventId_event_id` FOREIGN KEY(`event_id`) REFERENCES  `event`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  PRIMARY KEY(`id`)
+  `status` enum('REQUESTED','CONFIRMED') DEFAULT NULL,
+  `payment_details` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_learner_event` (`learner_id`, `event_id`),
+  KEY `FK_booking_learnerId_learner_id` (`learner_id`),
+  KEY `FK_booking_eventId_event_id` (`event_id`),
+  CONSTRAINT `FK_booking_eventId_event_id` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_booking_learnerId_learner_id` FOREIGN KEY (`learner_id`) REFERENCES `learner` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
