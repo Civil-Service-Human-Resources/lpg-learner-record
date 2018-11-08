a0629fbf2b5a2cfea07dc93babe69fec9044f639
Peter Taylor <petertaylor@Malekith.local>
2018-11-08 11:20:11 +0000

    LPFG-945: Added unqiue key to h2 and mysql

--- src/main/resources/db/migration/mysql/V1.1.6__add-invite-table.sql
+++ src/main/resources/db/migration/mysql/V1.1.6__add-invite-table.sql
@@ -3,5 +3,6 @@ CREATE TABLE `invite` (
   `event_id` int UNSIGNED NOT NULL,
   `learner_email` VARCHAR(50) NOT NULL,
   CONSTRAINT `FK_invite_event_id_event_id` FOREIGN KEY(`event_id`) REFERENCES `event`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  UNIQUE KEY (`event_id`, `learner_email`),
   PRIMARY KEY(`id`)
 )
