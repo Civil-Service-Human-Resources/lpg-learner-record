500961f8231671fbe21c56fd4b2c939e18170a8f
Yuksel Kurtbas <36735726+elxsy@users.noreply.github.com>
2018-11-13 10:51:21 +0000

    [BUG] Fix syntax errors

--- src/main/resources/db/migration/mysql/V1.1.5__add-event-learner-booking-tables.sql
+++ src/main/resources/db/migration/mysql/V1.1.5__add-event-learner-booking-tables.sql
@@ -1,24 +1,24 @@
 CREATE TABLE `event` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `event_uid` varchar(60) NOT NULL,
   `path` varchar(255) NOT NULL,
   PRIMARY KEY(`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `learner` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `uid` varchar(60) NOT NULL,
   PRIMARY KEY(`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `booking` (
-  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `learner_id` UNSIGNED int NOT NULL,
-  `event_id` UNSIGNED int NOT NULL,
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `learner_id` int(10) unsigned NOT NULL,
+  `event_id` int(10) unsigned NOT NULL,
   `booking_time` datetime NOT NULL,
   `status` enum('REQUESTED', 'CONFIRMED'),
   `payment_details` varchar(255),
   CONSTRAINT `FK_booking_learnerId_learner_id` FOREIGN KEY(`learner_id`) REFERENCES `learner`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `FK_booking_eventId_event_id` FOREIGN KEY(`event_id`) REFERENCES  `event`(`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY(`id`)
-);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
