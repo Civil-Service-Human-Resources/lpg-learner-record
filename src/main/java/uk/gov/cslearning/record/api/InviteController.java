960fd6297702e0062efdb560331f3e31aaf39388
Peter Taylor <petertaylor@Malekith.local>
2018-11-30 15:11:51 +0000

    LPFG-945: Re kick off jenkins

--- src/main/java/uk/gov/cslearning/record/api/InviteController.java
+++ src/main/java/uk/gov/cslearning/record/api/InviteController.java
@@ -1,6 +1,6 @@
 package uk.gov.cslearning.record.api;
 
- import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
