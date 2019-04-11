910459f64e4dcef51f6b948aa2eb152fe700dda2
matthewmu <matthewmu@kainos.com>
2019-04-11 14:47:04 +0100

    LPFG-1139-Data-Pruning-v2: env cosmos connection

--- src/main/java/uk/gov/cslearning/record/domain/Statements.java
+++ src/main/java/uk/gov/cslearning/record/domain/Statements.java
@@ -2,8 +2,14 @@ package uk.gov.cslearning.record.domain;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class Statements {
+    private String id;
+    private String personaIdentifier;
+    private List<String> activities;
+
     public Statements() {
     }
 }
