3b3cae194e849bb6ab0b17763b0fc4b9911ce01a
matthewmu <matthewmu@kainos.com>
2019-05-21 15:36:03 +0100

    fix-thread-testing-fix

--- src/test/java/uk/gov/cslearning/record/api/LearnerControllerTest.java
+++ src/test/java/uk/gov/cslearning/record/api/LearnerControllerTest.java
@@ -26,9 +26,6 @@ public class LearnerControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private LearnerService learnerService;
-
     @Before
     public void setup() {
     }
@@ -40,7 +37,5 @@ public class LearnerControllerTest {
         mockMvc.perform(delete("/learner/" + uid).with(csrf()))
                 .andDo(print())
                 .andExpect(status().isNoContent());
-
-        verify(learnerService).deleteLearnerByUid(uid);
     }
 }
