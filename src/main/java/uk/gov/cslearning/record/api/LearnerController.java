d64af3a1fef17db2b9715c57234596900f64c044
matthewmu <matthewmu@kainos.com>
2019-06-04 14:12:25 +0100

    removing manual tracking

--- src/main/java/uk/gov/cslearning/record/api/LearnerController.java
+++ src/main/java/uk/gov/cslearning/record/api/LearnerController.java
@@ -4,7 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import uk.gov.cslearning.record.service.LearnerService;
 
 @RestController
@@ -21,15 +24,11 @@ public class LearnerController {
     @DeleteMapping("/{uid}")
     @PreAuthorize("hasAnyAuthority('IDENTITY_DELETE')")
     public ResponseEntity deleteLearner(@PathVariable String uid) {
+        LOGGER.info("Deleting learner with uid {}", uid);
+
         learnerService.deleteLearnerByUid(uid);
 
-        return ResponseEntity.noContent().build();
-    }
-
-    @GetMapping("/track")
-    public ResponseEntity track() {
-        LOGGER.info("Deleting old statements");
-        learnerService.deleteOldStatements();
+        LOGGER.info("Learner deleted with uid {}", uid);
 
         return ResponseEntity.noContent().build();
     }
