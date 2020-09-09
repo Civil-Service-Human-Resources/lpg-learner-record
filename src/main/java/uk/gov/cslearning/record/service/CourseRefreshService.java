2ea5b8ccd71c202eb2b2a18f2a045978a0443e75
Matt Doherty <matthew.doherty@kainos.com>
2020-09-09 14:19:32 +0100

    [lc-653] - Remove transactional annotation on CourseRefreshService refreshCoursesForATimePeriod and replace with transaction management using transaction templates. It makes sense to keep the course refresh result in a seperate transaction, the result of the refresh is valid regardless of any errors further down the stream of processing, but if the transaction is opened at the start of processing the refresh login may take longer than the session timeout limit closing the connection. We could also avoid this by enabling autoreconnect or increasing the session timeout at the db level

--- src/main/java/uk/gov/cslearning/record/service/CourseRefreshService.java
+++ src/main/java/uk/gov/cslearning/record/service/CourseRefreshService.java
@@ -1,11 +1,13 @@
 package uk.gov.cslearning.record.service;
 
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.Collection;
-
 import gov.adlnet.xapi.model.Activity;
 import gov.adlnet.xapi.model.Statement;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 import uk.gov.cslearning.record.csrs.service.RegistryService;
 import uk.gov.cslearning.record.domain.CourseRecord;
 import uk.gov.cslearning.record.repository.CourseRecordRepository;
@@ -13,36 +15,44 @@ import uk.gov.cslearning.record.service.catalogue.LearningCatalogueService;
 import uk.gov.cslearning.record.service.xapi.StatementStream;
 import uk.gov.cslearning.record.service.xapi.XApiService;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Component
 public class CourseRefreshService {
+
+    private final TransactionTemplate transactionTemplate;
     private final CourseRecordRepository courseRecordRepository;
     private final LearningCatalogueService learningCatalogueService;
     private final RegistryService registryService;
     private final XApiService xApiService;
 
     @Autowired
-    public CourseRefreshService(CourseRecordRepository courseRecordRepository,
-            LearningCatalogueService learningCatalogueService,
-            RegistryService registryService,
-            XApiService xApiService) {
+    public CourseRefreshService(PlatformTransactionManager transactionManager,
+                                CourseRecordRepository courseRecordRepository,
+                                LearningCatalogueService learningCatalogueService,
+                                RegistryService registryService,
+                                XApiService xApiService) {
+        this.transactionTemplate = new TransactionTemplate(transactionManager);
         this.courseRecordRepository = courseRecordRepository;
         this.learningCatalogueService = learningCatalogueService;
         this.registryService = registryService;
         this.xApiService = xApiService;
     }
 
-    @Transactional
     public void refreshCoursesForATimePeriod(LocalDateTime since) {
         try {
             Collection<Statement> statements = xApiService.getStatements(null, null, since);
             StatementStream stream = new StatementStream(learningCatalogueService, registryService);
-            Collection<CourseRecord> updatedCourseRecords = stream.replay(statements,  statement -> ((Activity) statement.getObject()).getId());
+            Collection<CourseRecord> updatedCourseRecords = stream.replay(statements, statement -> ((Activity) statement.getObject()).getId());
 
-            courseRecordRepository.saveAll(updatedCourseRecords);
+            transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+                @Override
+                protected void doInTransactionWithoutResult(TransactionStatus status) {
+                    courseRecordRepository.saveAll(updatedCourseRecords);
+                }
+            });
         } catch (IOException e) {
             throw new RuntimeException("Exception retrieving xAPI statements.", e);
         }
