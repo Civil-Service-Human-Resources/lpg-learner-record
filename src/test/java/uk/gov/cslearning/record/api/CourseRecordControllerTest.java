59c311567d1d828f9b92c71a8a444efa48c5fe36
Jamie Lovell <jamie.lovell@cabinetoffice.gov.uk>
2021-09-09 02:17:30 +0100

    added tests for patchHelper and courseRecordController

--- src/test/java/uk/gov/cslearning/record/api/CourseRecordControllerTest.java
+++ src/test/java/uk/gov/cslearning/record/api/CourseRecordControllerTest.java
@@ -1,2 +1,85 @@
-package uk.gov.cslearning.record.api;public class CourseRecordControllerTest {
+package uk.gov.cslearning.record.api;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import org.bson.json.JsonParseException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import uk.gov.cslearning.record.domain.CourseRecord;
+import uk.gov.cslearning.record.dto.factory.ErrorDtoFactory;
+import uk.gov.cslearning.record.service.CourseRecordService;
+
+import java.io.IOException;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.powermock.api.mockito.PowerMockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest({CourseRecordController.class, ErrorDtoFactory.class})
+@WithMockUser(username = "user")
+public class CourseRecordControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private CourseRecordService courseRecordService;
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private JsonPatch generatePatch(String rawJson) throws IOException {
+        JsonNode patchJson = mapper.readTree(rawJson);
+        return JsonPatch.fromJson(patchJson);
+    }
+
+    private MockHttpServletRequestBuilder buildPatch(String content) {
+        MockHttpServletRequestBuilder builtPatch = patch("/course_records")
+                .with(csrf())
+                .contentType("application/json-patch+json")
+                .param("userId", "testUserId")
+                .param("courseId", "testCourseId")
+                .content(content);
+        return builtPatch;
+    }
+
+    @Test
+    public void testSuccessfulPatchInput() throws Exception {
+        CourseRecord sampleRecord = new CourseRecord();
+        String inputJson = "[{ \"op\": \"replace\", \"path\": \"/state\", \"value\": \"COMPLETED\" }\n]";
+        JsonPatch patchJson = generatePatch(inputJson);
+        when(courseRecordService.updateCourseRecord("testUserId", "testCourseId", patchJson))
+                .thenReturn(sampleRecord);
+        MockHttpServletRequestBuilder builtPatch = buildPatch(inputJson);
+        mockMvc.perform(builtPatch)
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testInvalidPatchOperation() throws Exception {
+        String inputJson = "[{ \"op\": \"invalid\", \"path\": \"/state\", \"value\": \"COMPLETED\" }\n]";
+        MockHttpServletRequestBuilder builtPatch = buildPatch(inputJson);
+        mockMvc.perform(builtPatch)
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void testInvalidContentType() throws Exception {
+        String inputJson = "[{ \"op\": \"replace\", \"path\": \"/state\", \"value\": \"COMPLETED\" }\n]";
+        JsonPatch patchJson = generatePatch(inputJson);
+        MockHttpServletRequestBuilder builtPatch = buildPatch(inputJson);
+        builtPatch.contentType("application/json");
+        mockMvc.perform(builtPatch)
+                .andExpect(status().isUnsupportedMediaType());
+    }
 }
--- src/test/java/uk/gov/cslearning/record/api/util/PatchHelperTest.java
+++ src/test/java/uk/gov/cslearning/record/api/util/PatchHelperTest.java
@@ -1,2 +1,91 @@
-package uk.gov.cslearning.record.api.util;public class PatchHelperTest {
+package uk.gov.cslearning.record.api.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.fge.jsonpatch.JsonPatch;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+import uk.gov.cslearning.record.config.patchHelper.PatchHelperConfig;
+import uk.gov.cslearning.record.exception.PatchResourceException;
+import uk.gov.cslearning.record.validation.annotations.ValidEnum;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import java.io.IOException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+enum FavouriteColor {
+    BLUE,
+    RED
+}
+
+@AllArgsConstructor
+@NoArgsConstructor
+class TestPerson {
+    public String name;
+    public int age;
+
+    @ValidEnum(enumClass = FavouriteColor.class)
+    @Enumerated(EnumType.STRING)
+    public String favouriteColor;
+}
+
+/**
+ * Test the PatchHelper class. Includes testing the objectmapper
+ * class as well.
+ */
+@RunWith(SpringRunner.class)
+public class PatchHelperTest {
+
+    private final PatchHelperConfig phc = new PatchHelperConfig();
+    private final PatchHelper patchHelper = new PatchHelper(phc.patchMapper());
+
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private JsonPatch generatePatch(String rawJson) throws IOException {
+        JsonNode patchJson = mapper.readTree(rawJson);
+        return JsonPatch.fromJson(patchJson);
+    }
+
+    private TestPerson getSamplePerson() {
+        return new TestPerson("John", 20, "BLUE");
+    }
+
+    @Test
+    public void testSuccessfulPatch() throws IOException {
+        TestPerson testPerson = getSamplePerson();
+        JsonPatch patch = generatePatch("[{ \"op\": \"replace\", \"path\": \"/name\", \"value\": \"Jack\" }]");
+        TestPerson updatedPerson = patchHelper.patch(patch, testPerson, TestPerson.class);
+        assertEquals("Jack", updatedPerson.name);
+    }
+
+    @Test
+    public void testValidationFailure() throws IOException {
+        TestPerson testPerson = getSamplePerson();
+        JsonPatch patch = generatePatch("[{ \"op\": \"replace\", \"path\": \"/favouriteColor\", \"value\": \"GREEN\" }]");
+
+        try {
+            patchHelper.patch(patch, testPerson, TestPerson.class);
+        } catch (Exception e) {
+            assertTrue(e instanceof PatchResourceException);
+        }
+    }
+
+    @Test
+    public void testInvalidFieldFailure() throws IOException {
+        TestPerson testPerson = getSamplePerson();
+        JsonPatch patch = generatePatch("[{ \"op\": \"replace\", \"path\": \"/favouriteFood\", \"value\": \"eggs\" }]");
+
+        try {
+            patchHelper.patch(patch, testPerson, TestPerson.class);
+        } catch (Exception e) {
+            assertTrue(e instanceof PatchResourceException);
+        }
+    }
+
 }
