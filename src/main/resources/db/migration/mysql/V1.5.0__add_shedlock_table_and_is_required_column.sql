9f5df0d2302e34b890a9ca845fb1d41551bf99db
Jamie Lovell <jamie.lovell@cabinetoffice.gov.uk>
2021-09-21 11:13:05 +0100

    revert mistakenly commited files

--- src/main/resources/db/migration/mysql/V1.5.0__add_shedlock_table_and_is_required_column.sql
+++ src/main/resources/db/migration/mysql/V1.5.0__add_shedlock_table_and_is_required_column.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `shedlock` (
   `name` VARCHAR(64) NOT NULL,
-  `lock_until` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `locked_at` TIMESTAMP NOT NULL DEFAULT NOW(),
-  `locked_by` VARCHAR(255) NOT NULL DEFAULT NOW(),
+  `lock_until` TIMESTAMP NOT NULL,
+  `locked_at` TIMESTAMP NOT NULL,
+  `locked_by` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
