215c3455653d29d4542f012b31dcd308491d3bf2
jaylco <77386599+jaylco@users.noreply.github.com>
2022-08-09 08:30:17 +0100

    Extra event fields for module patch

--- src/main/java/uk/gov/cslearning/record/api/input/PATCH/PatchModuleRecordInput.java
+++ src/main/java/uk/gov/cslearning/record/api/input/PATCH/PatchModuleRecordInput.java
@@ -37,12 +37,19 @@ public class PatchModuleRecordInput {
 
     private boolean rated;
 
+    //TODO: This field is never used
     private String paymentMethod;
 
+    //TODO: This field is never used
     private String paymentDetails;
 
     @ValidEnum(enumClass = BookingStatus.class)
     @Enumerated(EnumType.STRING)
     private String bookingStatus;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    private LocalDateTime eventDate;
+
+    private String eventId;
 
 }
