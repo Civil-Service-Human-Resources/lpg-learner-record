d874a3d401b5816237413deb5540442c68dea980
jaylco <77386599+jaylco@users.noreply.github.com>
2022-09-30 09:49:05 +0100

    Removed unused Mongo gradle packages, updated Docker java base image to amazoncorretto

--- src/test/java/uk/gov/cslearning/record/api/CourseRecordControllerTest.java
+++ src/test/java/uk/gov/cslearning/record/api/CourseRecordControllerTest.java
@@ -3,7 +3,6 @@ package uk.gov.cslearning.record.api;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
-import org.bson.json.JsonParseException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
