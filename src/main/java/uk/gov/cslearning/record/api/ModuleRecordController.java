1976be2f59fc8a7f78caaef61ceb5087db85e794
Neeraj Jain <neeraj.jain1@cabinetoffice.gov.uk>
2023-02-06 17:51:46 +0000

    LC-1627 adding debug logs

--- src/main/java/uk/gov/cslearning/record/api/ModuleRecordController.java
+++ src/main/java/uk/gov/cslearning/record/api/ModuleRecordController.java
@@ -26,18 +26,8 @@ public class ModuleRecordController {
     public ResponseEntity<ModuleRecord> updateModuleRecord(@PathVariable("moduleRecordId") Long moduleRecordId,
                                                            @RequestBody JsonPatch patchData) {
         log.debug("LC-1627: ModuleRecordController.updateModuleRecord.received moduleRecordId: {} ", moduleRecordId);
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.received patchData: {} ", patchData);
         ModuleRecord updatedRecord = moduleRecordService.updateModuleRecord(moduleRecordId, patchData);
         log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord: {} ", updatedRecord);
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getId: {} ", updatedRecord.getId());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getModuleId: {} ", updatedRecord.getModuleId());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getCreatedAt: {} ", updatedRecord.getCreatedAt());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getUpdatedA: {} ", updatedRecord.getUpdatedAt());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getCompletionDate: {} ", updatedRecord.getCompletionDate());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getState: {} ", updatedRecord.getState());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getResult: {} ", updatedRecord.getResult());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getScore: {} ", updatedRecord.getScore());
-        log.debug("LC-1627: ModuleRecordController.updateModuleRecord.returning updatedRecord.getModuleTitle: {} ", updatedRecord.getModuleTitle());
         return new ResponseEntity<>(updatedRecord, HttpStatus.OK);
     }
 
