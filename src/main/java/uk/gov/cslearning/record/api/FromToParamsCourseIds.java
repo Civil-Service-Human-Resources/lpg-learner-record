c33c65624621ba8ab126e460b43c48c35d7b4634
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2025-05-08 13:25:03 +0100

    fix module record report

---
+++ src/main/java/uk/gov/cslearning/record/api/FromToParamsCourseIds.java
@@ -0,0 +1,18 @@
+package uk.gov.cslearning.record.api;
+
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class FromToParamsCourseIds extends FromToParams {
+
+    @Size(min = 1, max = 100)
+    private List<String> courseIds;
+
+}
--- src/main/java/uk/gov/cslearning/record/repository/ModuleRecordRepository.java
+++ src/main/java/uk/gov/cslearning/record/repository/ModuleRecordRepository.java
@@ -1,7 +1,6 @@
 package uk.gov.cslearning.record.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import uk.gov.cslearning.record.domain.ModuleRecord;
 
@@ -10,28 +9,9 @@ import java.util.List;
 
 @Repository
 public interface ModuleRecordRepository extends JpaRepository<ModuleRecord, Long> {
-    @Query("SELECT mr " +
-            "FROM ModuleRecord mr " +
-            "left join CourseRecord cr on cr.identity.courseId = mr.courseRecord.identity.courseId " +
-            "WHERE mr.updatedAt BETWEEN ?1 AND ?2 " +
-            "AND mr.courseRecord IS NOT NULL")
-    List<ModuleRecord> findAllByCreatedAtBetweenAndCourseRecordIsNotNullNormalised(LocalDateTime from, LocalDateTime to);
+    List<ModuleRecord> findAllByUpdatedAtBetween(LocalDateTime from, LocalDateTime to);
 
-    @Query("SELECT mr " +
-            "FROM ModuleRecord mr " +
-            "left join CourseRecord cr on cr.identity.courseId = mr.courseRecord.identity.courseId " +
-            "WHERE mr.updatedAt BETWEEN ?1 AND ?2 " +
-            "AND mr.courseRecord.identity.userId in (?3) " +
-            "AND mr.courseRecord IS NOT NULL " +
-            "ORDER BY mr.courseRecord.identity.userId")
-    List<ModuleRecord> findForLearnerIdsByCreatedAtBetweenAndCourseRecordIsNotNullNormalised(LocalDateTime from, LocalDateTime to, List<String> learnerIds);
+    List<ModuleRecord> findAllByUpdatedAtBetweenAndCourseRecord_Identity_UserIdIn(LocalDateTime from, LocalDateTime to, List<String> learnerIds);
 
-    @Query("SELECT mr " +
-            "FROM ModuleRecord mr " +
-            "left join CourseRecord cr on cr.identity.courseId = mr.courseRecord.identity.courseId " +
-            "WHERE mr.updatedAt BETWEEN ?1 AND ?2 " +
-            "AND mr.courseRecord.identity.courseId in (?3) " +
-            "AND mr.courseRecord IS NOT NULL " +
-            "ORDER BY mr.courseRecord.identity.userId")
-    List<ModuleRecord> findForCourseIdsByCreatedAtBetweenAndCourseRecordIsNotNullNormalised(LocalDateTime from, LocalDateTime to, List<String> courseIds);
+    List<ModuleRecord> findAllByUpdatedAtBetweenAndCourseRecord_Identity_CourseIdIn(LocalDateTime from, LocalDateTime to, List<String> courseIds);
 }
