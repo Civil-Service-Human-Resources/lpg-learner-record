818822f0312caf4df34510da24345fa96680ec9e
Jamie <jamie.lovell@cabinetoffice.gov.uk>
2025-05-20 10:29:27 +0100

    Tweak SQL column

--- src/main/resources/db/migration/h2/V2.2.0__event-source-uid.sql
+++ src/main/resources/db/migration/h2/V2.2.0__event-source-uid.sql
@@ -1,8 +1,8 @@
-ALTER TABLE learner_record.learner_record_event_sources
+ALTER TABLE learner_record_event_sources
     ADD COLUMN uid VARCHAR(50);
 
-UPDATE learner_record.learner_record_event_sources
+UPDATE learner_record_event_sources
 SET uid = 'dummy';
 
-ALTER TABLE learner_record.learner_record_event_sources
+ALTER TABLE learner_record_event_sources
     MODIFY COLUMN uid varchar(50) NOT NULL;
