e358991a0cfcb45320da09c94fbee39d27807a98
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-12-19 15:04:25 +0000

    Correct SQL

--- src/main/resources/db/migration/mysql/V2.4.0__learner_uid_bookings.sql
+++ src/main/resources/db/migration/mysql/V2.4.0__learner_uid_bookings.sql
@@ -8,7 +8,7 @@ update booking b JOIN learner l on (b.learner_id = l.id)
 set b.learner_uid = l.uid;
 
 update invite i JOIN learner l on (i.learner_email = l.learner_email)
-set i.learner_uid = i.uid;
+set i.learner_uid = l.uid;
 
 ALTER TABLE booking
     MODIFY COLUMN learner_uid varchar(60) NOT NULL;
