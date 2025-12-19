0bbc05e944cc909780f4179f41fdbd8432f79b38
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-12-19 15:41:32 +0000

    Delete learner ID FK on booking table

--- src/main/resources/db/migration/h2/V2.5.0__delete_event_path.sql
+++ src/main/resources/db/migration/h2/V2.5.0__delete_event_path.sql
@@ -2,6 +2,9 @@ ALTER TABLE event
     DROP COLUMN `path`;
 
 ALTER TABLE booking
+    DROP FOREIGN KEY FK_booking_learnerId_learner_id;
+
+ALTER TABLE booking
     DROP COLUMN `learner_id`;
 
 DROP TABLE learner;
