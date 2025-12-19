61c7e4bc9ed518641716ab652b6e2dd32384676e
Jamie <jamie.lovell@cabinetofice.gov.uk>
2025-12-19 15:26:58 +0000

    Delete null columns

--- src/main/resources/db/migration/mysql/V2.4.0__learner_uid_bookings.sql
+++ src/main/resources/db/migration/mysql/V2.4.0__learner_uid_bookings.sql
@@ -10,6 +10,14 @@ set b.learner_uid = l.uid;
 update invite i JOIN learner l on (i.learner_email = l.learner_email)
 set i.learner_uid = l.uid;
 
+DELETE
+FROM booking
+where learner_uid IS NULL;
+
+DELETE
+FROM invite
+where learner_uid IS NULL;
+
 ALTER TABLE booking
     MODIFY COLUMN learner_uid varchar(60) NOT NULL;
 
